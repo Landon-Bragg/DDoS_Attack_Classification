46893f0e553f784c97202dc713308e7b522649ff
Landon Bragg <ldbragg04@gmail.com>
2025-04-25 09:13:35 -0700

    First One

---
+++ config.py
@@ -0,0 +1,29 @@
+CONFIG = {
+    # Paths
+    "data_root": "preprocessed_dataset",
+    "output_dir": "outputs",
+
+    # Training DO NOT GO ABOVE 16
+    "batch_size": 16,
+    "epochs": 3,
+    "lr": 5e-5,
+    "num_workers": 4,
+    "pin_memory": True,
+    "persistent_workers": True,
+
+    # Adversarial parameters
+    "fgsm_eps": 1.26,
+    "pgd_eps": 1.215,
+    "pgd_alpha": 1.1, # look into this
+    # The higher you make this value, the slower everything takes to run 
+    "pgd_steps": 40,
+    # For some reason if you run less batches, it does worse on clean data but better on augmented and pgd
+    "max_eval_batches": 100,
+    "max_train_batches": None,
+
+    # Model
+    "num_classes": 2,
+
+    # Random seed
+    "seed": 42,
+}
